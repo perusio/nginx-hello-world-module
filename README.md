6e79835d19431c17e9f8a07e2082e9344afa1ec4
António P. P. Almeida <appa@perusio.net>
2011-08-17 16:45:23 +0100

    * Initial commit.

---
+++ README.md
@@ -0,0 +1,73 @@
+# Hello World Module for Nginx
+
+## Introduction
+
+This module serves as a learning exercise for me, and hopefully for
+others too, when doing [Nginx](http://nginx.org) module development. 
+
+I [stole](http://dominicfallows.com/2011/02/20/hello-world-nginx-module-3/)
+the code and added some notes using mostly Evan Miller's
+[Nginx Module Development Guide](http://www.evanmiller.org/nginx-modules-guide.html). Also
+helpful is the
+[translation](http://antoine.bonavita.free.fr/nginx_mod_dev_en.html)
+of Vhalery Kholodov's
+[Nginx Module Guide](http://www.grid.net.ru/nginx/nginx-modules.html)
+done by [Antoine Bonavita](http://antoine.bonavita.free.fr/) that also
+mantains a [Nginx Discovery](http://www.nginx-discovery.com/) blog to
+document his journey on Nginx module development.
+
+## Installation
+
+   1. Configure Nginx adding this module with:
+          
+          ./configure (...) --add-module=/path/to/nginx-hello-world-module
+       
+   2. Build Nginx as usual with `make`.
+   
+   3. Configure the module. There's only one directive `hello_world`
+      that is supported in the **location** context only.
+      
+      Example:
+          
+          location = /test {
+             
+             hello_world;
+          
+          }
+
+      Now doing something like:
+          
+          curl -i http://example.com/test
+          
+      should return the **hello world** string as the response body.
+
+## Further reading
+
+ * English: You can go to the [Nginx Planet](http://planet.nginx.org/)
+   and get a lot of Nginx related information.
+  
+ * Russian: The [Habrahabr Nginx Blog](habrahabr.ru/blogs/nginx/) a
+   treasure trove of Nginx related discussions. Use google translate
+   or Babelfish if you don't read Russian.
+      
+## TODO
+
+ 1. Add an argument to the `hello_world` directive specifying the
+    language. For example:
+    
+    * en: hello world
+    
+    * fr: bounjour monde
+    
+    * pt: olá mundo
+ 
+    * es: hola mundo
+ 
+ 2. Make the `hello_world` directive be also defined in the **if in
+    location** context (`NGX_HTTP_LIF_CONF`) in
+    `ngx_http_config.h`. Which implies defining a **merge
+    configuration** function.
+ 
+ 3. Document everything with
+    [Doxygen](https://secure.wikimedia.org/wikipedia/en/wiki/Doxygen)
+    linking the relevant header and source files from the Nginx core.
---
+++ ngx_http_hello_world_module.c
@@ -0,0 +1,150 @@
+/**
+ * @file   ngx_http_hello_world_module.c
+ * @author António P. P. Almeida <appa@perusio.net>
+ * @date   Wed Aug 17 12:06:52 2011
+ *
+ * @brief  A hello world module for Nginx.
+ *
+ * @section LICENSE
+ *
+ * Copyright (C) 2011 by Dominic Fallows, António P. P. Almeida <appa@perusio.net>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ */
+#include <ngx_config.h>
+#include <ngx_core.h>
+#include <ngx_http.h>
+
+
+#define HELLO_WORLD "hello world"
+
+static char *ngx_http_hello_world(ngx_conf_t *cf, ngx_command_t *cmd, void *conf);
+static ngx_int_t ngx_http_hello_world_handler(ngx_http_request_t *r);
+
+/**
+ * This module provided directive: hello world.
+ *
+ */
+static ngx_command_t ngx_http_hello_world_commands[] = {
+
+    { ngx_string("hello_world"), /* directive */
+      NGX_HTTP_LOC_CONF|NGX_CONF_NOARGS, /* location context and takes
+                                            no arguments*/
+      ngx_http_hello_world, /* configuration setup function */
+      0, /* No offset. Only one context is supported. */
+      0, /* No offset when storing the module configuration on struct. */
+      NULL},
+
+    ngx_null_command /* command termination */
+};
+
+/* The hello world string. */
+static u_char ngx_hello_world[] = HELLO_WORLD;
+
+/* The module context. */
+static ngx_http_module_t ngx_http_hello_world_module_ctx = {
+    NULL, /* preconfiguration */
+    NULL, /* postconfiguration */
+
+    NULL, /* create main configuration */
+    NULL, /* init main configuration */
+
+    NULL, /* create server configuration */
+    NULL, /* merge server configuration */
+
+    NULL, /* create location configuration */
+    NULL /* merge location configuration */
+};
+
+/* Module definition. */
+ngx_module_t ngx_http_hello_world_module = {
+    NGX_MODULE_V1,
+    &ngx_http_hello_world_module_ctx, /* module context */
+    ngx_http_hello_world_commands, /* module directives */
+    NGX_HTTP_MODULE, /* module type */
+    NULL, /* init master */
+    NULL, /* init module */
+    NULL, /* init process */
+    NULL, /* init thread */
+    NULL, /* exit thread */
+    NULL, /* exit process */
+    NULL, /* exit master */
+    NGX_MODULE_V1_PADDING
+};
+
+/**
+ * Content handler.
+ *
+ * @param r
+ *   Pointer to the request structure. See http_request.h.
+ * @return
+ *   The status of the response generation.
+ */
+static ngx_int_t ngx_http_hello_world_handler(ngx_http_request_t *r)
+{
+    ngx_buf_t *b;
+    ngx_chain_t out;
+
+    /* Set the Content-Type header. */
+    r->headers_out.content_type.len = sizeof("text/plain") – 1;
+    r->headers_out.content_type.data = (u_char *) "text/plain";
+
+    /* Allocate a new buffer for sending out the reply. */
+    b = ngx_pcalloc(r->pool, sizeof(ngx_buf_t));
+
+    /* Insertion in the buffer chain. */
+    out.buf = b;
+    out.next = NULL; /* just one buffer */
+
+    b->pos = ngx_hello_world; /* first position in memory of the data */
+    b->last = ngx_hello_world + sizeof(ngx_hello_world); /* last position in memory of the data */
+    b->memory = 1; /* content is in read-only memory */
+    b->last_buf = 1; /* there will be no more buffers in the request */
+
+    /* Sending the headers for the reply. */
+    r->headers_out.status = NGX_HTTP_OK; /* 200 status code */
+    /* Get the content length of the body. */
+    r->headers_out.content_length_n = sizeof(ngx_hello_world);
+    ngx_http_send_header(r); /* Send the headers */
+
+    /* Send the body, and return the status code of the output filter chain. */
+    return ngx_http_output_filter(r, &out);
+} /* ngx_http_hello_world_handler */
+
+/**
+ * Configuration setup function that installs the content handler.
+ *
+ * @param cf
+ *   Module configuration structure pointer.
+ * @param cmd
+ *   Module directives structure pointer.
+ * @param conf
+ *   Module configuration structure pointer.
+ * @return string
+ *   Status of the configuration setup.
+ */
+static char *ngx_http_hello_world(ngx_conf_t *cf, ngx_command_t *cmd, void *conf)
+{
+    ngx_http_core_loc_conf_t *clcf; /* pointer to core location configuration */
+
+    /* Install the hello world handler. */
+    clcf = ngx_http_conf_get_module_loc_conf(cf, ngx_http_core_module);
+    clcf->handler = ngx_http_hello_world_handler;
+
+    return NGX_CONF_OK;
+} /* ngx_http_hello_world */
